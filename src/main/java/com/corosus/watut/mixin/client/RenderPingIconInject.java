857042126579c509775f6a5c8e0da6fa58c155a0
Corosauce <cordonfreeman@gmail.com>
2025-03-08 19:49:23 +0000

    Undo test change.

--- src/main/java/com/corosus/watut/mixin/client/RenderPingIconInject.java
+++ src/main/java/com/corosus/watut/mixin/client/RenderPingIconInject.java
@@ -20,7 +20,7 @@ public abstract class RenderPingIconInject {
             at = @At(value = "INVOKE",
                     target = "Lnet/minecraft/client/gui/components/PlayerTabOverlay;renderPingIcon(Lnet/minecraft/client/gui/GuiGraphics;IIILnet/minecraft/client/multiplayer/PlayerInfo;)V"))
     public void renderPingIcon(PlayerTabOverlay playerTabOverlay, GuiGraphics pGuiGraphics, int p_281809_, int p_282801_, int pY, PlayerInfo pPlayerInfo) {
-        if (WatutMod.getPlayerStatusManagerClient().renderPingIconHook((PlayerTabOverlay)(Object)this, pGuiGraphics, p_281809_, p_282801_, pY, pPlayerInfo)) {
+        if (!WatutMod.getPlayerStatusManagerClient().renderPingIconHook((PlayerTabOverlay)(Object)this, pGuiGraphics, p_281809_, p_282801_, pY, pPlayerInfo)) {
             playerTabOverlay.renderPingIcon(pGuiGraphics, p_281809_, p_282801_, pY, pPlayerInfo);
         }
     }
